0fe4c7317c0a255d336f7ac719e7c5e2a1e191e9
sanjeet kumar <sanjeet.kumar2@delhivery.com>
2020-09-08 12:15:04 +0530

    Updates

---
+++ precache-manifest.0b82ab4dda4a041b6cbfb3d076ae1bc8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86189f13bf6ec385afe05c27c23eb6fb",
+    "url": "/practice-proj3/index.html"
+  },
+  {
+    "revision": "4991eea5b284771336f5",
+    "url": "/practice-proj3/static/css/2.90929467.chunk.css"
+  },
+  {
+    "revision": "7e1d4fe6f2b354d5ad8e",
+    "url": "/practice-proj3/static/css/main.6a7cf207.chunk.css"
+  },
+  {
+    "revision": "4991eea5b284771336f5",
+    "url": "/practice-proj3/static/js/2.06f4baf8.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/practice-proj3/static/js/2.06f4baf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1d4fe6f2b354d5ad8e",
+    "url": "/practice-proj3/static/js/main.d6a27ff7.chunk.js"
+  },
+  {
+    "revision": "2571cec7757a6342bdab",
+    "url": "/practice-proj3/static/js/runtime-main.b03fed3b.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/practice-proj3/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/practice-proj3/static/media/logo.5d5d9eef.svg"
+  }
+]);
